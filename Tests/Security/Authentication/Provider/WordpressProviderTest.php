8d988d7f921b59215ec3a0d4df33e9fdb3596d59
Ka Yue Yeung <kayuey@gmail.com>
2013-05-11 14:53:46 +0800

    Updates tests.

--- Tests/Security/Authentication/Provider/WordpressProviderTest.php
+++ Tests/Security/Authentication/Provider/WordpressProviderTest.php
@@ -3,88 +3,40 @@
 namespace Kayue\WordpressBundle\Tests\Security\Authentication\Provider;
 
 use Kayue\WordpressBundle\Security\Authentication\Provider\WordpressProvider;
-use Kayue\WordpressBundle\Security\Authentication\Token\WordpressToken;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class WordpressProviderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testNotSupportedToken()
+    public function testAuthenticateWithAnonymousToken()
     {
-        $userProvider = $this->getMockBuilder('Symfony\Component\Security\Core\User\UserProviderInterface')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $authenticationProvider = new WordpressProvider('key', 'salt', $userProvider);
-        $token = $this->getMock('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
-        $this->assertNull($authenticationProvider->authenticate($token));
-    }
-
-    public function testExpiredToken()
-    {
-        $userProviderMock = $this->getMock('Symfony\Component\Security\Core\User\UserProviderInterface');
-        $tokenMock = $this->getMockBuilder('Kayue\WordpressBundle\Security\Authentication\Token\WordpressToken')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $tokenMock->expects($this->once())
-            ->method('isExpired')
-            ->will($this->returnValue(true));
-
-        $authenticationProvider = new WordpressProvider('key', 'salt', $userProviderMock);
-
-        try {
-            $authenticationProvider->authenticate($tokenMock);
-        } catch(AuthenticationException $e) {
-            $this->assertEquals('The WordPress login cookie has expired.', $e->getMessage());
-        }
-    }
-
-    public function testUserNotFound()
-    {
-        $userProviderMock = $this->getMock('Symfony\Component\Security\Core\User\UserProviderInterface');
-        $userProviderMock->expects($this->once())
-            ->method('loadUserByUsername')
-            ->with('mary')
-            ->will($this->returnValue(null));
-
-        $authenticationProvider = new WordpressProvider('key', 'salt', $userProviderMock);
-
-        try {
-            $authenticationProvider->authenticate(new WordpressToken('mary', time()+1000, 'hmac'));
-        } catch(AuthenticationException $e) {
-            $this->assertEquals('Invalid WordPress login cookie, user doesn\'t exist.', $e->getMessage());
-        }
-    }
-
-    public function testAuthenticate()
-    {
-        $userMock = $this->getMock('Symfony\Component\Security\Core\User\UserInterface');
-        $userMock->expects($this->any())
-            ->method('getUsername')
-            ->will($this->returnValue('admin'));
-        $userMock->expects($this->any())
-            ->method('getPassword')
-            ->will($this->returnValue('$P$BLfimWPNEsJCMh1NyKprIBjHIYtBbu/'));
-
-        $userProviderMock = $this->getMock('Symfony\Component\Security\Core\User\UserProviderInterface');
-        $userProviderMock->expects($this->once())
-            ->method('loadUserByUsername')
-            ->with('admin')
-            ->will($this->returnValue($userMock));
-
-        $tokenMock = $this->getMock('Kayue\WordpressBundle\Security\Authentication\Token\WordpressToken',
-            ['isExpired'],
-            ['admin','1366613007','8092029696a419b5b024fbe90eb61888']
+        $userCheckerMock = $this->getUserCheckerMock();
+        $tokenMock = $this->getMock(
+            'Symfony\Component\Security\Core\Authentication\Token\AnonymousToken',
+            array(),
+            array('key', 'user', array())
         );
-        $tokenMock->expects($this->once())
-            ->method('isExpired')
-            ->will($this->returnValue(false));
+        $wordpressProvider = new WordpressProvider($userCheckerMock);
 
-        $key = ':j$_=(:l@8Fku^U;MQ~#VOJXOZcVB_@u+t-NNYqmTH4na|)5Bhs1|tF1IA|>tz*E';
-        $salt = ')A^CQ<R:1|^dK/Q;.QfP;U!=J=(_i6^s0f#2EIbGIgFN{,3U9H$q|o/sJfWF`NRM';
-        $authenticationProvider = new WordpressProvider($key, $salt, $userProviderMock);
+        $this->assertNull($wordpressProvider->authenticate($tokenMock));
+    }
 
-        $newToken = $authenticationProvider->authenticate($tokenMock);
+    public function testAuthenticateWithWordpressToken()
+    {
+        $userCheckerMock = $this->getUserCheckerMock();
+        $userMock = $this->getMock('Symfony\Component\Security\Core\User\UserInterface');
+        $tokenMock = $this->getMock(
+            'Kayue\WordpressBundle\Security\Authentication\Token\WordpressToken',
+            array('getUser'),
+            array($userMock, array())
+        );
+        $tokenMock->expects($this->any())->method('getUser')->will($this->returnValue($userMock));
 
-        $this->assertTrue($newToken->isAuthenticated());
+        $wordpressProvider = new WordpressProvider($userCheckerMock);
+
+        $this->assertTrue($wordpressProvider->authenticate($tokenMock)->isAuthenticated());
+    }
+
+    private function getUserCheckerMock()
+    {
+        return $this->getMock('Symfony\Component\Security\Core\User\UserCheckerInterface');
     }
 }
