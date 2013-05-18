a03b53fc412c4d1ba3670cfe24d9459effb4cf83
Ka Yue Yeung <kayuey@gmail.com>
2013-05-18 19:22:38 +0800

    Keep WordpressEntityManager::create() compatible with its parent.
    Declaration of
    Kayue\WordpressBundle\Doctrine\WordpressEntityManager::create() should
    be compatible with that of Doctrine\ORM\EntityManager::create()

--- Doctrine/WordpressEntityManager.php
+++ Doctrine/WordpressEntityManager.php
@@ -2,8 +2,8 @@
 
 namespace Kayue\WordpressBundle\Doctrine;
 
+use Doctrine\Common\EventManager;
 use Doctrine\ORM\Configuration;
-use Doctrine\ORM\ORMException;
 
 class WordpressEntityManager extends \Doctrine\ORM\EntityManager
 {
@@ -31,9 +31,10 @@ class WordpressEntityManager extends \Doctrine\ORM\EntityManager
      * @param mixed $conn An array with the connection parameters or an existing
      *      Connection instance.
      * @param Configuration $config The Configuration instance to use.
+     * @param EventManager $eventManager The EventManager instance to use.
      * @return WordpressEntityManager The created EntityManager.
      */
-    public static function create($conn, Configuration $config)
+    public static function create($conn, Configuration $config, EventManager $eventManager = null)
     {
         return new WordpressEntityManager($conn, $config, $conn->getEventManager());
     }
