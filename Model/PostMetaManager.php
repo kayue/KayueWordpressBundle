6f6da32fb3ecb6564e80b6648aa08d9fd8f31c4e
Peter Wong <peter.wong@101medialab.com>
2013-05-23 12:21:47 +0800

    Implement Post Meta Manager

---
+++ Model/PostMetaManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Kayue\WordpressBundle\Model;
+
+use Kayue\WordpressBundle\Entity\Post;
+use Kayue\WordpressBundle\Entity\PostMeta;
+
+class PostMetaManager implements PostManagerInterface
+{
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * Constructor.
+     *
+     * @param EntityManager     $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+        $this->repository = $em->getRepository('KayueWordpressBundle:Post');
+    }
+
+    public function addMeta(Post $post, PostMeta $meta)
+    {
+        $post->addMeta($meta);
+    }
+
+    public function saveMeta(PostMeta $meta)
+    {
+        // do nothing
+    }
+
+    public function deleteMeta(PostMeta $meta)
+    {
+        $em->delete($meta);
+    }
+
+    public function findAllMetasByPost(Post $post)
+    {
+        return $post->getMetas();
+    }
+
+    public function findMetasBy(array $criteria)
+    {
+        return $this->repository->findBy($criteria);
+    }
+
+    public function findOneMetaBy(array $criteria)
+    {
+        return $this->repository->findOneBy($criteria);
+    }
+}
