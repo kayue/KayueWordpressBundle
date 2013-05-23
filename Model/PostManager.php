cfa796f25be419457a8d2670307a101ad80d3f63
Peter Wong <peter.wong@101medialab.com>
2013-05-23 11:46:01 +0800

    Complete Post Manager

---
+++ Model/PostManager.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kayue\WordpressBundle\Model;
+
+class PostManager implements PostManagerInterface
+{
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var EntityRepository
+     */
+    protected $repository;
+
+    /**
+     * Constructor.
+     *
+     * @param EntityManager     $em
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em         = $em;
+        $this->repository = $em->getRepository('KayueWordpressBundle:Post');
+    }
+
+    public function findPostById($id)
+    {
+        return $this->repository->findBy(array(
+            'id' => $id,
+        ));
+    }
+
+    public function findPostBySlug($slug)
+    {
+        return $this->repository->findBy(array(
+            'slug' => $slug,
+        ));
+    }
+}
