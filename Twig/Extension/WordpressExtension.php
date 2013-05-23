18525cc13dd380c442cef3d726e50798cd8daefd
Peter Wong <peter.wong@101medialab.com>
2013-05-23 13:22:21 +0800

    Twig: remove blog manager, fix findTermsByPost param

--- Twig/Extension/WordpressExtension.php
+++ Twig/Extension/WordpressExtension.php
@@ -4,6 +4,7 @@ namespace Kayue\WordpressBundle\Twig\Extension;
 
 use Doctrine\ORM\EntityManager;
 use Kayue\WordpressBundle\Entity\Post;
+use Kayue\WordpressBundle\Entity\Taxonomy;
 use Kayue\WordpressBundle\Model\AttachmentManager;
 use Kayue\WordpressBundle\Model\BlogManager;
 use Kayue\WordpressBundle\Model\OptionManager;
@@ -17,17 +18,15 @@ class WordpressExtension extends \Twig_Extension
     protected $em;
 
     protected $attachmentManager;
-    protected $blogManager;
     protected $optionManager;
     protected $postManager;
     protected $postMetaManager;
     protected $termManager;
 
-    public function __construct(EntityManager $em, Container $container)
+    public function __construct(EntityManager $em)
     {
         $this->em = $em;
         $this->attachmentManager = new AttachmentManager($em);
-        $this->blogManager = new BlogManager($container);
         $this->optionManager = new OptionManager($em);
         $this->postManager = new PostManager($em);
         $this->postMetaManager = new PostMetaManager($em);
@@ -45,7 +44,6 @@ class WordpressExtension extends \Twig_Extension
             'wp_find_attachments_by_post' => new \Twig_Function_Method($this, 'findAttachmentsByPost'),
             'wp_find_one_attachment_by_id' => new \Twig_Function_Method($this, 'findOneAttachmentById'),
             'wp_find_feature_image_by_post' => new \Twig_Function_Method($this, 'findFeatureImageByPost'),
-            'wp_find_blog_by_id' => new \Twig_Function_Method($this, 'findBlogById'),
             'wp_find_one_option_by_name' => new \Twig_Function_Method($this, 'findOneOptionByName'),
             'wp_find_post_by_id' => new \Twig_Function_Method($this, 'findPostById'),
             'wp_find_post_by_slug' => new \Twig_Function_Method($this, 'findPostBySlug'),
@@ -69,11 +67,6 @@ class WordpressExtension extends \Twig_Extension
     public function findFeatureImageByPost(Post $post, $size = null)
     {
         return $this->attachmentManager->findFeatureImageByPost($post, $size);
-    }
-
-    public function findBlogById($id)
-    {
-        return $this->blogManager->findBlogById($id);
     }
 
     public function findOneOptionByName($id)
@@ -106,8 +99,9 @@ class WordpressExtension extends \Twig_Extension
         return $this->postMetaManager->findOneMetaBy($criteria);
     }
 
-    public function findTermsByPost($id)
+    public function findTermsByPost(Post $post, Taxonomy $taxonomy = null)
     {
-        return $this->termManager->findTermsByPost($id);
+        return $this->termManager->findTermsByPost($id, $taxonomy);
     }
+
 }
