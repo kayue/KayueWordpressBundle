ca5dc2a8c525f97e97302017a9e226c57ba4cc50
Ka Yue Yeung <kayuey@gmail.com>
2013-06-02 11:46:30 +0800

    Add user meta model.

--- Entity/UserMeta.php
+++ Entity/UserMeta.php
@@ -3,6 +3,7 @@
 namespace Kayue\WordpressBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Kayue\WordpressBundle\Model\UserMeta as ModelUserMeta;
 use Symfony\Component\Validator\Constraints as Constraints;
 
 /**
@@ -11,101 +12,41 @@ use Symfony\Component\Validator\Constraints as Constraints;
  * @ORM\Table(name="usermeta")
  * @ORM\Entity
  */
-class UserMeta
+class UserMeta extends ModelUserMeta
 {
     /**
-     * @var int $id
+     * {@inheritdoc}
      *
      * @ORM\Column(name="umeta_id", type="bigint", length=20)
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    private $id;
+    protected $id;
 
     /**
-     * @var string $key
+     * {@inheritdoc}
      *
      * @ORM\Column(name="meta_key", type="string", length=255, nullable=true)
      * @Constraints\NotBlank()
      */
-    private $key;
+    protected $key;
 
     /**
-     * @var string $value
+     * {@inheritdoc}
      *
      * @ORM\Column(name="meta_value", type="wordpressmeta", nullable=true)
      */
-    private $value;
+    protected $value;
 
     /**
-     * @var \Kayue\WordpressBundle\Entity\User
+     * {@inheritdoc}
      *
      * @ORM\ManyToOne(targetEntity="Kayue\WordpressBundle\Entity\User", inversedBy="metas")
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="user_id", referencedColumnName="ID")
      * })
      */
-    private $user;
-
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set key
-     *
-     * @param string $key
-     */
-    public function setKey($key)
-    {
-        $this->key = $key;
-    }
-
-    /**
-     * Get key
-     *
-     * @return string
-     */
-    public function getKey()
-    {
-        return $this->key;
-    }
-
-    /**
-     * Set value
-     *
-     * @param string $value
-     */
-    public function setValue($value)
-    {
-        $this->value = $value;
-    }
-
-    /**
-     * Get value
-     *
-     * @return string
-     */
-    public function getValue()
-    {
-        return $this->value;
-    }
-
-    /**
-     * Set user
-     *
-     * @param \Kayue\WordpressBundle\Entity\User $user
-     */
-    public function setUser(User $user)
-    {
-        $this->user = $user;
-    }
+    protected $user;
 
     /**
      * Get user
