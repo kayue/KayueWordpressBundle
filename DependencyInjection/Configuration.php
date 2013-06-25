bc25433d137bacdec9c178b4d3d2651af44fe450
Ka Yue Yeung <kayuey@gmail.com>
2013-06-25 22:28:15 +0800

    Add cache drivers to configuration.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Kayue\WordpressBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -27,8 +28,45 @@ class Configuration implements ConfigurationInterface
             ->scalarNode('cookie_path')->defaultValue('/')->end()
             ->scalarNode('cookie_domain')->defaultValue(null)->end()
             ->scalarNode('table_prefix')->defaultValue('wp_')->end()
+            ->arrayNode('orm')
+                ->append($this->getCacheDriverNode('query_cache_driver'))
+                ->append($this->getCacheDriverNode('metadata_cache_driver'))
+                ->append($this->getCacheDriverNode('result_cache_driver'))
+            ->end()
         ->end();
 
         return $treeBuilder;
+    }
+
+
+    /**
+     * Return a ORM cache driver node for an given entity manager
+     *
+     * @param string $name
+     *
+     * @return ArrayNodeDefinition
+     */
+    private function getCacheDriverNode($name)
+    {
+        $treeBuilder = new TreeBuilder();
+        $node = $treeBuilder->root($name);
+
+        $node
+            ->addDefaultsIfNotSet()
+            ->beforeNormalization()
+                ->ifString()
+                ->then(function($v) { return array('type' => $v); })
+            ->end()
+            ->children()
+                ->scalarNode('type')->defaultValue('array')->end()
+                ->scalarNode('host')->end()
+                ->scalarNode('port')->end()
+                ->scalarNode('instance_class')->end()
+                ->scalarNode('class')->end()
+                ->scalarNode('id')->end()
+            ->end()
+        ;
+
+        return $node;
     }
 }
