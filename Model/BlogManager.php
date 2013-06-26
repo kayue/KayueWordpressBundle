0caa2162b677c05d0bd9586d6493f901cc806b03
Ka Yue <kayuey@gmail.com>
2013-06-26 14:52:48 +0800

    Comma bug fix.

--- Model/BlogManager.php
+++ Model/BlogManager.php
@@ -51,9 +51,9 @@ class BlogManager implements BlogManagerInterface
                 $this->container->get('database_connection'), $baseConfig
             );
 
-            $em->getMetadataFactory()->setCacheDriver($this->getCacheImpl('metadata_cache'), $id);
-            $em->getConfiguration()->setQueryCacheImpl($this->getCacheImpl('query_cache'), $id);
-            $em->getConfiguration()->setResultCacheImpl($this->getCacheImpl('result_cache'), $id);
+            $em->getMetadataFactory()->setCacheDriver($this->getCacheImpl('metadata_cache', $id));
+            $em->getConfiguration()->setQueryCacheImpl($this->getCacheImpl('query_cache', $id));
+            $em->getConfiguration()->setResultCacheImpl($this->getCacheImpl('result_cache', $id));
 
             try {
                 if (null === $em->getRepository('KayueWordpressBundle:Blog')->findOneBy(array('id'=>$id))) {
