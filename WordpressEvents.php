5647e3d2164ac2e0a00856f9e631c49c2bc6a10c
umpirsky <umpirsky@gmail.com>
2014-11-12 16:46:59 +0100

    Create new entty manager event

--- WordpressEvents.php
+++ WordpressEvents.php
@@ -10,4 +10,11 @@ class WordpressEvents
      * @var string
      */
     const SWITCH_BLOG = 'wordpress.blog.switch_blog';
+
+    /**
+     * Create new entty manager.
+     *
+     * @var string
+     */
+    const CREATE_ENTITY_MANAGER = 'wordpress.entity_manager.create';
 }
