becbcce411fc0d39bb6e810b52ee395985c02675
Ka Yue Yeung <kayuey@gmail.com>
2014-11-21 10:19:08 +0800

    Add getMetaByPost method.

--- Repository/PostMetaRepository.php
+++ Repository/PostMetaRepository.php
@@ -21,6 +21,20 @@ class PostMetaRepository extends AbstractRepository
         ;
     }
 
+    public function getMetaByPost(Post $post, $key, $hydrationMode = AbstractQuery::HYDRATE_ARRAY)
+    {
+        return $this->getQueryBuilder()
+            ->join('pm.post', 'post')
+            ->andWhere('post.id = :postId')
+            ->andWhere('pm.key = :key')
+            ->setMaxResults(1)
+            ->setParameter('postId', $post->getId())
+            ->setParameter('key', $key)
+            ->getQuery()
+            ->getSingleResult($hydrationMode)
+        ;
+    }
+
     public function getAlias()
     {
         return 'pm';
